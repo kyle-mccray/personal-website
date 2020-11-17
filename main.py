162700e9cc70654f3c2656adb187045d1cf1b7ca
Kyle <kcmsonic@gmail.com>
2020-11-17 11:09:05 -0600

    updated app to force https in prod

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ if __name__ == "__main__":
     app.run()
 
 
+@app.before_request
+def before_request():
+    if not request.is_secure and app.env != "development":
+        url = request.url.replace("http://", "https://", 1)
+        code = 301
+        return redirect(url, code=code)
+
+
 @app.route('/')
 def index():
     return render_template('frontPage.html')
